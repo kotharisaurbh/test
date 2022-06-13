d39fe34e29373ef0c0424b696abe5f1da31e3e93
kotharisaurbh <kotharisaurabh31@gmail.com>
2022-06-14 00:55:13 +0530

    Update request.php

--- request.php
+++ request.php
@@ -2,5 +2,5 @@
 header('Content-type: text/xml');
 ?>
 <Response>
-    <Dial callerId="+15703619011"><?php  echo $_POST['To'];?></Dial>
+    <Dial callerId="+18158531305"><?php  echo $_POST['To'];?></Dial>
 </Response>
